304a6abae127b046dbd06714dda0bb3d4cdc3243
M@teo <mateusz.pawlicki@hotmail.com>
2017-01-03 20:01:27 +0100

    add source and cmake files

---
+++ src/Stack.h
@@ -0,0 +1,9 @@
+#ifndef __STACK_H___
+#define __STACK_H___
+
+class Stack
+{
+	
+};
+
+#endif
---
+++ test/test.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE StackTest
+#include <boost/test/unit_test.hpp>
+
+#include "Stack.h"
+
+BOOST_AUTO_TEST_CASE(FailTest)
+{
+    BOOST_CHECK_EQUAL(5, 2);
+}
