1b62770da7438a179e4bad432ec5741797beabad
M@teo <mateusz.pawlicki@hotmail.com>
2017-02-12 09:56:53 +0000

    added implementation for mem-leak check in valgrind

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include "Stack.hpp"
+#include "List.hpp"
 #include <iostream>
 
 int main(int argc,char* argv[])
 {
 	mpb::Stack<int> inStack{};
+	mpb::List<int> myList;
 
 	auto inc = 100 * 100 * 1000;
 	for (auto i = 0; i < inc; ++i)
@@ -13,7 +15,18 @@ int main(int argc,char* argv[])
 
 	for (auto i = 0; i < inc; ++i)
 	{
-		std::cout << inStack.pop() << ', ';
+		inStack.pop();
+	}
+
+	std::cout <<std::endl;
+
+	for(auto i = 0; i < inc; ++i)
+	{
+		myList.push_back(i);
+	}
+	for(auto i = 0; i < inc; ++i)
+	{
+		myList.pop_front();
 	}
 	return 0;
 }
