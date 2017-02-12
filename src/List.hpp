97f610da6dc3922c3a2543a1c9d24319f59eb1eb
M@teo <mateusz.pawlicki@hotmail.com>
2017-02-12 10:41:21 +0100

    change list impl to template

--- src/List.hpp
+++ src/List.hpp
@@ -5,54 +5,240 @@
 
 namespace mpb
 {
-	class ListException : std::logic_error
-	{
-	public:
-		ListException(const std::string& what_arg);
-		~ListException();
-		const char* what() const noexcept override;
-	private:
-		std::string message;
-	};
-
-	class List
-	{
-
-	public:
-		struct Node
-		{
-			int value;
-			Node* next = nullptr;
-			Node(int _value, Node* node);
-		};
-	    List();
-		List(std::initializer_list<int> values);
-		~List();
-
-		List(const List& lList);
-		List& operator=(const List& lList);
-
-		List(List&& rList);
-		List& operator=(List&& rList);
-
-        void push_back(int value);
-		int pop_back();
-
-		void push_front(int value);
-		int pop_front();
-		Node* find(int valueToFind);
-		inline bool empty() const
-		{
-            return nullptr == head && count == 0 ? true : false;
-		}
-		inline int countElements() const
-		{
-			return count;
-		}
+    class ListException : std::logic_error
+    {
+    public:
+        ListException(const std::string& what_arg);
+        ~ListException();
+        const char* what() const noexcept override;
     private:
-        Node* head;
-		Node* tail;
-		int count = 0;
-	};
+        std::string message;
+    };
+
+    template<typename T>
+    struct Node
+    {
+        T value;
+        Node<T>* next = nullptr;
+        Node() = default;
+    };
+    
+
+    template<typename T>
+    class List
+    {
+
+    public:
+
+
+        List();
+
+        List(std::initializer_list<T> values);
+        ~List();
+        List(const List<T>& lList);
+        List<T>& operator=(const List<T>& lList);
+        List(List<T>&& rList);
+        List<T>& operator=(List<T>&& rList);
+
+        void push_back(T value);
+        T pop_back();
+        void push_front(T value);
+        T pop_front();
+
+        /*Node<T>* find(T valueToFind);*/
+
+        inline bool empty() const;
+        inline int countElements() const;
+    private:
+        Node<T>* head;
+        Node<T>* tail;
+        int count = 0;
+    };
+
+
+    template<typename T>
+    inline bool List<T>::empty() const
+    {
+        return nullptr == head && count == 0 ? true : false;
+    }
+
+    template<typename T>
+    inline int List<T>::countElements() const
+    {
+        return count;
+    }
+
+
+    template<typename T>
+    List<T>::List() : head{ nullptr }, tail{ nullptr }
+    {
+
+    }
+
+    template<typename T>
+    List<T>::List(std::initializer_list<T> values) : List<T>()
+    {
+        for (const auto& x : values)
+        {
+            push_back(x);
+        }
+    }
+
+    template<typename T>
+    List<T>::~List()
+    {
+        while (head != nullptr)
+        {
+            Node<T>* tmp = head;
+            head = tmp->next;
+            delete tmp;
+        }
+    }
+
+    template<typename T>
+    List<T>::List(const List<T>& lList) : List<T>()
+    {
+        Node<T>* temp = lList.head;
+        while (temp)
+        {
+            push_back(temp->value);
+            temp = temp->next;
+        }
+    }
+
+    template<typename T>
+    List<T>& List<T>::operator=(const List<T>& lList)
+    {
+        Node<T>* temp = lList.head;
+        while (temp)
+        {
+            push_back(temp->value);
+            temp = temp->next;
+        }
+        return *this;
+    }
+
+    template<typename T>
+    List<T>::List(List<T>&& rList) : head{ rList.head }, tail{ rList.tail }, count{ rList.count }
+    {
+        rList.head = nullptr;
+        rList.tail = nullptr;
+        rList.count = 0;
+    }
+
+    template<typename T>
+    List<T>& List<T>::operator=(List<T>&& rList)
+    {
+        head = rList.head;
+        rList.head = nullptr;
+        tail = rList.tail;
+        rList.tail = nullptr;
+        count = rList.count;
+        rList.count = 0;
+        return *this;
+    }
+
+    template<typename T>
+    void List<T>::push_back(T value)
+    {
+        if (nullptr == head)
+        {
+            Node<T>* newHead = new Node<T>;
+            newHead->value = value;
+            newHead->next = nullptr;
+            head = newHead;
+            tail = head;
+            ++count;
+            return;
+        }
+        Node<T>* newNext = new Node<T>;
+        newNext->value = value;
+        newNext->next = nullptr;
+        tail->next = newNext;
+        tail = tail->next;
+        ++count;
+    }
+
+    template<typename T>
+    T List<T>::pop_back()
+    {
+        T retValue;
+        if (empty())
+        {
+            throw ListException("Can't pop from empty list");
+        }
+        if (head == tail)
+        {
+            retValue = head->value;
+            delete head;
+            head = tail = nullptr;
+            --count;
+            return retValue;
+        }
+
+        Node<T>* current = head;
+        while (current->next != tail)
+        {
+            current = current->next;
+        }
+        retValue = tail->value;
+        delete tail;
+        tail = current;
+        tail->next = nullptr;
+        --count;
+        return retValue;
+    }
+
+    template<typename T>
+    void List<T>::push_front(T value)
+    {
+        if (nullptr == head)
+        {
+            
+            Node<T>* newNode = new Node<T>;
+            newNode->value = value;
+            newNode->next = head;
+            tail = head = newNode;
+            ++count;
+            return;
+        }
+        Node<T>* newNode = new Node<T>;
+        newNode->value = value;
+        newNode->next = head;
+        head = newNode;
+        ++count;
+    }
+
+    template<typename T>
+    T List<T>::pop_front()
+    {
+        if (!head)
+            throw ListException("Can't pop from empty list");
+        Node<T>* newHead = head->next;
+        T retValue = head->value;
+        delete head;
+        head = newHead;
+        --count;
+        return retValue;
+    }
+
+    /*template<typename T>
+    List<T>::Node<T>* List<T>::find(T valueToFind)
+    {
+        Node<T>* current = head;
+        while (nullptr != current)
+        {
+            if (valueToFind != current->value)
+            {
+                current = current->next;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return current;
+    }*/
+
 }
 #endif
--- test/list_test.cpp
+++ test/list_test.cpp
@@ -5,7 +5,7 @@ BOOST_AUTO_TEST_SUITE(List)
 
 BOOST_AUTO_TEST_CASE(List_empty)
 {
-    mpb::List myList;
+    mpb::List<int> myList;
     myList.push_back(10);
 	
 	BOOST_CHECK_EQUAL(10, myList.pop_back());
@@ -13,7 +13,7 @@ BOOST_AUTO_TEST_CASE(List_empty)
 
 BOOST_AUTO_TEST_CASE(List_PushWithoutPop)
 {
-	mpb::List myList;
+	mpb::List<int> myList;
 	myList.push_back(10);
 	myList.push_back(21);
 
@@ -23,27 +23,27 @@ BOOST_AUTO_TEST_CASE(List_PushWithoutPop)
 
 BOOST_AUTO_TEST_CASE(List_FindElement)
 {
-	mpb::List myList;
+	//mpb::List<int> myList;
 
-	for (auto i = 0; i < 10; ++i)
-	{
-		myList.push_back(i);
-	}
-	mpb::List::Node* tmp = myList.find(8);
-	BOOST_CHECK(tmp != nullptr);
-	BOOST_CHECK_EQUAL( 8, tmp->value);
-	BOOST_CHECK(!myList.empty());
+	//for (auto i = 0; i < 10; ++i)
+	//{
+	//	myList.push_back(i);
+	//}
+	//mpb::List<int>::Node<int>* tmp = myList.find(8);
+	//BOOST_CHECK(tmp != nullptr);
+	//BOOST_CHECK_EQUAL( 8, tmp->value);
+	//BOOST_CHECK(!myList.empty());
 }
 
 BOOST_AUTO_TEST_CASE(List_Empty)
 {
-	mpb::List myList;
+	mpb::List<int> myList;
 	BOOST_CHECK(myList.empty());
 }
 
 BOOST_AUTO_TEST_CASE(List_Count)
 {
-	mpb::List myList;
+	mpb::List<int> myList;
 
 	for (auto i = 0; i < 100; ++i)
 	{
@@ -54,7 +54,7 @@ BOOST_AUTO_TEST_CASE(List_Count)
 
 BOOST_AUTO_TEST_CASE(List_CreateAndPopElements)
 {
-	mpb::List list{ { 10,15,20,25,30 } };
+	mpb::List<int> list{ { 10,15,20,25,30 } };
 	
 	BOOST_CHECK_EQUAL(30, list.pop_back());
 	BOOST_CHECK_EQUAL(25, list.pop_back());
@@ -65,8 +65,8 @@ BOOST_AUTO_TEST_CASE(List_CreateAndPopElements)
 
 BOOST_AUTO_TEST_CASE(List_CopyList)
 {
-	mpb::List list{ { 10,15,20,25,30 } };
-	mpb::List newList(list);
+	mpb::List<int> list{ { 10,15,20,25,30 } };
+	mpb::List<int> newList(list);
 
 	BOOST_CHECK_EQUAL(30, list.pop_back());
 	BOOST_CHECK_EQUAL(25, list.pop_back());
@@ -83,8 +83,8 @@ BOOST_AUTO_TEST_CASE(List_CopyList)
 
 BOOST_AUTO_TEST_CASE(List_CopyList_CopyAssignment)
 {
-	mpb::List list{ { 10,15,20,25,30 } };
-	mpb::List newList;
+	mpb::List<int> list{ { 10,15,20,25,30 } };
+	mpb::List<int> newList;
 	newList = list;
 
 	BOOST_CHECK_EQUAL(30, list.pop_back());
@@ -102,8 +102,8 @@ BOOST_AUTO_TEST_CASE(List_CopyList_CopyAssignment)
 
 BOOST_AUTO_TEST_CASE(List_MoveList)
 {
-	mpb::List list{ { 10,15,20,25,30 } };
-	mpb::List newList(std::move(list));
+	mpb::List<int> list{ { 10,15,20,25,30 } };
+	mpb::List<int> newList(std::move(list));
 
 	BOOST_CHECK_EQUAL(30, newList.pop_back());
 	BOOST_CHECK_EQUAL(25, newList.pop_back());
@@ -114,8 +114,8 @@ BOOST_AUTO_TEST_CASE(List_MoveList)
 
 BOOST_AUTO_TEST_CASE(List_ListMoveAssignment)
 {
-	mpb::List list{ { 10,15,20,25,30 } };
-	mpb::List newList;
+	mpb::List<int> list{ { 10,15,20,25,30 } };
+	mpb::List<int> newList;
 
 	newList = std::move(list);
 
@@ -128,7 +128,7 @@ BOOST_AUTO_TEST_CASE(List_ListMoveAssignment)
 
 BOOST_AUTO_TEST_CASE(List_pushFront)
 {
-	mpb::List list{ { 10,15,20,25,30 } };
+	mpb::List<int> list{ { 10,15,20,25,30 } };
 	
 	list.push_front(1);
 
@@ -142,7 +142,7 @@ BOOST_AUTO_TEST_CASE(List_pushFront)
 
 BOOST_AUTO_TEST_CASE(List_pushFrontOneElement)
 {
-	mpb::List list{};
+	mpb::List<int> list{};
 
 	list.push_front(1);
 	BOOST_CHECK_EQUAL(1, list.pop_back());
@@ -150,7 +150,7 @@ BOOST_AUTO_TEST_CASE(List_pushFrontOneElement)
 
 BOOST_AUTO_TEST_CASE(List_popBackFromEmpty)
 {
-	mpb::List list{};
+	mpb::List<int> list{};
 
 	BOOST_CHECK_EQUAL(true, list.empty());
     BOOST_CHECK_THROW(list.pop_back(), mpb::ListException);
@@ -158,7 +158,7 @@ BOOST_AUTO_TEST_CASE(List_popBackFromEmpty)
 
 BOOST_AUTO_TEST_CASE(List_popFrontFromEmpty)
 {
-    mpb::List list{};
+    mpb::List<int> list{};
 
     BOOST_CHECK_EQUAL(true, list.empty());
     BOOST_CHECK_THROW(list.pop_front(), mpb::ListException);
@@ -166,7 +166,7 @@ BOOST_AUTO_TEST_CASE(List_popFrontFromEmpty)
 
 BOOST_AUTO_TEST_CASE(List_popFront)
 {
-	mpb::List list{ { 10,15,20,25,30 } };
+	mpb::List<int> list{ { 10,15,20,25,30 } };
 
 	BOOST_CHECK_EQUAL(10, list.pop_front());
 	BOOST_CHECK_EQUAL(15, list.pop_front());
@@ -178,7 +178,7 @@ BOOST_AUTO_TEST_CASE(List_popFront)
 
 BOOST_AUTO_TEST_CASE(List_pushFrontOneElementAndPopFront)
 {
-    mpb::List list;
+    mpb::List<int> list;
 
     list.push_front(30);
     BOOST_CHECK_EQUAL(30, list.pop_front());
